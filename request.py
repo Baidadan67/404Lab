3fd4ce0c2944b8987429baacfdfe4d9eeb289f31
Baidadan67 <82280462+Baidadan67@users.noreply.github.com>
2022-09-15 20:02:49 -0600

    Update request.py

--- request.py
+++ request.py
@@ -2,3 +2,8 @@ import requests
 #print(requests.__version__)
 
 print(requests.get("http://www.google.com/"))
+
+raw_url = "https://raw.githubusercontent.com/Baidadan67/404Lab1/main/request.py"
+
+res = requests.get(raw_url)
+print(res.text)
